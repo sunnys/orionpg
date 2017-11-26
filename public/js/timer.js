13e5a5fc770f53e4b6cd33e2722eb7e81a06c1ea
sunnys <sunny@ptotem.com>
2017-11-26 22:43:44 +0530

    added this new date in timer

--- public/js/timer.js
+++ public/js/timer.js
@@ -10,10 +10,10 @@ time zone offset.
 */
 
 var current="Winter is here!";   //-->enter what you want the script to display when the target date and time are reached, limit to 20 characters
-var year=2019;    //-->Enter the count down target date YEAR
+var year=2018;    //-->Enter the count down target date YEAR
 var month=1;      //-->Enter the count down target date MONTH
 var day=1;       //-->Enter the count down target date DAY
-var hour=12;      //-->Enter the count down target date HOUR (24 hour clock)
+var hour=00;      //-->Enter the count down target date HOUR (24 hour clock)
 var minute=00;    //-->Enter the count down target date MINUTE
 var tz=-5;        //-->Offset for your timezone in hours from UTC (see http://wwp.greenwichmeantime.com/index.htm to find the timezone offset for your location)
 
