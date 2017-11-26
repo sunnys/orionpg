77c3b79b1fb53a69972e17a6ade3199b439d8525
sunnys <sunny@ptotem.com>
2017-11-26 23:14:34 +0530

    added cors setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200'
+    # origins 'localhost:4200'
+    origins 'www.sunnyssingh.in'
 
     resource '*',
       headers: :any,
